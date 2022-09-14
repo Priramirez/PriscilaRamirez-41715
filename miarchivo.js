729c3e579ba6c822eb34c001cbd650324e022f33
Priramirez <112518184+Priramirez@users.noreply.github.com>
2022-09-13 22:30:44 -0300

    Add files via upload

--- miarchivo.js
+++ miarchivo.js
@@ -17,32 +17,73 @@ function saludar () {
 
 saludar ()
 
-
-
-function calcularPrecio ( producto, precio, cantidad ) {
-    
-    let subtotal = (precio * cantidad);
-    let impuestoAldeano = (subtotal * 0.21);
-    let total = (subtotal + impuestoAldeano);
-        
-    if (confirm(`Usted esta comprando:
-    
-          ${cantidad} Dragon ${producto} 
-    
-          subtotal: $${subtotal} gemas
-          total: ${total} gemas 
-    
-          ¿Desea continuar?`)) {
-
-         if (confirm('Presione "continuar" para concretar su compra')) {
-            alert(`El dragón ${producto} esta en camino`);
-         } else {
-            alert('hemos cancelado su compra');
-        }
-     }
-     else {
-        alert ('compra cancelada');
-     }
+class Dragon {
+    constructor(producto, precio, cantidad) {
+        this.producto = producto;
+        this.precio = precio;
+    }
 }
 
-    
+
+const dragones = [] ;  
+
+const dragon1 = new Dragon('Áragon', 180);
+const dragon2 = new Dragon('Cynder', 1000);
+const dragon3 = new Dragon('Plutonius', 200);
+
+dragones.push (dragon1, dragon2, dragon3);
+
+console.log (dragones);
+
+let carrito = [];
+let dragon = "";
+let precio = 0;
+let agregarItems = false;
+let cantidad = 0;
+let total = 0;
+
+do {
+    dragon = prompt('Qué dragon desea adquirir?: ').toUpperCase();
+    cantidad = prompt('Cuántos dragones llevará?: ');
+
+    switch(dragon) {
+        case "ÁRAGON" :
+        precio = 180;
+        break;
+
+        case "ARAGON" :
+        precio = 180;
+        break;
+
+        case "CYNDER" :
+        precio = 1000;
+        break;
+
+        case "PLUTONIUS" :
+        precio = 200;
+        break;
+
+        default :
+        alert('No contamos con el dragón mencionado. Por favor, escriba otro nombre')
+        precio = 0;
+        cantidad = 0;
+
+    }
+
+
+agregarItems = confirm("Desea agregar más productos al carrito?");
+
+carrito.push({dragon,cantidad,precio});
+
+}
+while(agregarItems);
+
+if (confirm('Desea revisar su compra?')) {
+    for (const compra of carrito) {
+        alert('Dragón: '+compra.dragon+
+        ' Cantidad: '+compra.cantidad+
+        ' Monto final: '+compra.cantidad*compra.precio+' gemas');
+    }
+} else {
+    alert ('Gracias por su compra');
+}
